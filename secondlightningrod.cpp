b8fbb49492b204c54c6b91134c858e7bdacff1ff
Alexandr <sashatlt8@gmail.com>
2020-12-16 23:23:00 +0400

    all predusmotheyshon

--- secondlightningrod.cpp
+++ secondlightningrod.cpp
@@ -13,7 +13,41 @@ double secondLightningRod::fullHeightB(double hx, double rx) {
 double secondLightningRod::fullHeightA(double hx, double rx) {
     double h1 = 270 - 2 * sqrt(25 * (729 - 4 * rx) - 158 * hx);
     double h2 = 2 * (sqrt(25 * (729 - 4 * rx) - 158 * hx) + 135);
-    //Некая логика по выбору корня
+    if(h1 < 0){
+        if(h2 > 0){
+            if (h2 > hx){
+                return h2;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    } else {
+        if (h2 < 0) {
+            return h1;
+        } else {
+            if(h1 < hx) {
+                if (h2 > hx) {
+                    return h2;
+                } else {
+                    return 0;
+                }
+            } else{
+                if (h2 < hx) {
+                    return h1;
+                } else {
+                    if (h2 > h1){
+                        return h2;
+                    } else {
+                        return h1;
+                    }
+                }
+            }
+        }
+    }
+
+
     return 0;
 }
 
